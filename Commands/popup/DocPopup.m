b60f06ce37ff916efff47583e6839daa17b506e0
Joachim Mårtensson <joachimm@c-68e2e055.129-2-64736c10.cust.bredbandsbolaget.se>
2010-06-05 14:07:01 +0200

    add the missing files

---
+++ Commands/popup/DocPopup.m
@@ -0,0 +1,48 @@
+//
+//  DocPopup.m
+//  Dialog2
+//
+//  Created by Joachim Mårtensson on 2009-01-11.
+//  Copyright 2009 Chalmers. All rights reserved.
+//
+
+#import "DocPopup.h"
+
+
+@implementation DocPopup
++ (id)showWithContent:(NSString*)content atLocation:(NSPoint)point transparent:(BOOL)transparent
+{
+	DocPopup* tip = [DocPopup new];
+	[tip setFrameTopLeftPoint:point];
+	[tip setContent:content transparent:transparent];
+	return tip;
+}
+
+- (id)init
+{
+    if( (self = [super init]) ) {
+
+    }
+    return self;
+}
+- (void)runUntilUserActivity
+{
+	return;
+}
+- (BOOL)shouldCloseForMousePosition:(NSPoint)aPoint
+{
+	return NO;
+}
+- (void) close 
+{
+	[webView setFrameLoadDelegate:nil];
+	[[NSNotificationCenter defaultCenter] removeObserver:self name:NSViewFrameDidChangeNotification object:self];
+	[super close];
+}
+
+- (void)webView:(WebView*)sender didFinishLoadForFrame:(WebFrame*)frame;
+{
+		[self sizeToContent];
+		[self orderFront:self];
+	}
+@end
