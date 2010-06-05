74adf48769bc3df2a2fbb85f456402710020edf7
Joachim Mårtensson <joachimm@c-68e2e055.129-2-64736c10.cust.bredbandsbolaget.se>
2010-06-05 12:40:40 +0200

    Fixed build issues on GCC 4.2, and made GCC 4.0 explicit so as to not confuse Snow Leopard users

--- TMDCommand.h
+++ TMDCommand.h
@@ -3,9 +3,7 @@
 @interface TMDCommand : NSObject
 + (void)registerObject:(id)anObject forCommand:(NSString*)aCommand;
 + (NSDictionary *)registeredCommands;
-
 + (id)objectForCommand:(NSString*)aCommand;
-
 + (id)readPropertyList:(NSFileHandle*)aFileHandle error:(NSString**)error;
 + (void)writePropertyList:(id)aPlist toFileHandle:(NSFileHandle*)aFileHandle;
 
